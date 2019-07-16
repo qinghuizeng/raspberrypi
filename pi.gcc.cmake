28b9ccc39f36f8993a50ec377705f1ce075b84ca
vince.zeng <vince.zeng@arm.com>
2019-07-16 09:32:38 +0800

    - Add CMAKE_TOOLCHIAN_FILE for cross compiling. - Add sample code

---
+++ pi.gcc.cmake
@@ -0,0 +1,8 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_C_COMPILER $ENV{HOME}/workspace_raspberry_pi/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER $ENV{HOME}/workspace_raspberry_pi/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/workspace_raspberry_pi/rootfs)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
